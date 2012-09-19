5a9d5e15a674ecb1da670680a1608e0eff6b5368
Kasper Johansen <k@spernj.org>
2012-09-19 16:33:39 +0200

    Added a new threadding module.

---
+++ lib/gtk3assist_threadding.rb
@@ -0,0 +1,37 @@
+class Gtk3assist::Threadding
+  #Enables other threads to be run and interrupts to be used (<CTRL> + <C>).
+  def self.enable_threadding(args = nil)
+    #Never start this more than once.
+    return nil if @enable_threadding
+    @enable_threadding = true
+    
+    #Amount of time to check if a new thread wants to run.
+    if args and args[:sleep]
+      @time = args[:sleep]
+    else
+      @time = 100
+    end
+    
+    #Call the method (which then calls itself based on timings).
+    enable_threadding_pass
+  end
+  
+  private
+  
+  #Passes to another running thread. Based on the time something else is running, the pass will be called again as soon as possible or waiting a small amount of time to check again if not.
+  def self.enable_threadding_pass(*args)
+    t_begin = Time.now.to_f
+    Thread.pass
+    t_run = Time.now.to_f - t_begin
+    
+    if t_run < 0.0001
+      #Run again after a small amount of time to prevent 100% CPU.
+      GLib.timeout_add(GLib::PRIORITY_DEFAULT_IDLE, @time, self.method(:enable_threadding_pass), nil, nil)
+      return false
+    else
+      #Run again on next idle.
+      GLib.idle_add(GLib::PRIORITY_DEFAULT_IDLE, self.method(:enable_threadding_pass), nil, nil)
+      return false
+    end
+  end
+end
